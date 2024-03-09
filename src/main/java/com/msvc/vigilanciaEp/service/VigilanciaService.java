1b0c4a119f624c7b80346268d570bd173ac45a71
Gerardo <gerardo20005232@gmail.com>
2024-03-09 15:11:22 -0600

    cambio Gerardo

--- src/main/java/com/msvc/vigilanciaEp/service/VigilanciaService.java
+++ src/main/java/com/msvc/vigilanciaEp/service/VigilanciaService.java
@@ -22,7 +22,7 @@ public class VigilanciaService {
     /*INYECTAMOS LOS REPOSITORIOS*/
     @Autowired
     private VigilanciaRepository vigilanciaRepository;
-//bola bolaMovil
+//bola bolaMovil bola
     private static final Logger logger = Logger.getLogger(VigilanciaService.class.getName());
 
     public void guardarMes(Vigilancia vigilancia) {
