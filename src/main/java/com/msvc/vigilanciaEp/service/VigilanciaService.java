93115e2727b19d57351cb47c1b10e28a473fc4fa
pabloT0807 <digital38@inter.mx>
2024-03-09 15:17:34 -0600

    pablo cambio

--- src/main/java/com/msvc/vigilanciaEp/service/VigilanciaService.java
+++ src/main/java/com/msvc/vigilanciaEp/service/VigilanciaService.java
@@ -28,7 +28,7 @@ public class VigilanciaService {
     public void guardarMes(Vigilancia vigilancia) {
         vigilanciaRepository.save(vigilancia);
     }
-//vola
+//soy pablo
     public List<Vigilancia> buscarPorNombreAlcaldia(String alcaldia) {
         return vigilanciaRepository.findByAlcaldia(alcaldia);
     }
